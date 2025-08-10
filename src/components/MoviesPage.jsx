f9daaff7bef7ed588216de17fd214950f70e89aa
nirch <nirh2@yahoo.com>
2025-08-10 09:52:13 +0300

    controlled component + state

--- src/components/MoviesPage.jsx
+++ src/components/MoviesPage.jsx
@@ -6,18 +6,43 @@ import { useState } from "react";
 function MoviesPage() {
   const [movies, setMovies] = useState(jsonMovies);
   const [comedyOnly, setComedyOnly] = useState(false);
+  const [filterText, setFilterText] = useState("Fo");
+
 
   let displayMovies = movies;
-  if (comedyOnly) {
-    displayMovies = displayMovies.filter(movie => movie.genres.includes('Comedy'));
+  filterMovies();
+
+  function filterMovies() {
+    if (comedyOnly) {
+      displayMovies = displayMovies.filter((movie) =>
+        movie.genres.includes("Comedy")
+      );
+    }
+
+    if (filterText) {
+      displayMovies = displayMovies.filter(movie => movie.name.toLowerCase().includes(filterText.toLowerCase()))
+    }
   }
+
 
   return (
     <div className="movies-page">
       <h1>Movies Page</h1>
-      <button onClick={() => {setComedyOnly(!comedyOnly)}}>
-        {comedyOnly ? "Show All" : "Comedy Only"}
-      </button>
+      <div className="filter-movies">
+        <input
+          type="text"
+          placeholder="Filter Movies..."
+          value={filterText}
+          onChange={(e) => setFilterText(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setComedyOnly(!comedyOnly);
+          }}
+        >
+          {comedyOnly ? "Show All" : "Comedy Only"}
+        </button>
+      </div>
       {displayMovies.map((movie) => (
         <Movie movie={movie} key={movie.id} />
       ))}
