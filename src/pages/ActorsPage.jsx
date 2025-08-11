d98e0f1ac0f5370d6c2eff116df0cb6ae4b526c5
nirch <nirh2@yahoo.com>
2025-08-11 14:38:31 +0300

    adding mantine to project

---
+++ src/pages/ActorsPage.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mantine/core";
+
+function ActorsPage() {
+  return (
+    <Container size="md">
+      <h1>Actors Page</h1>
+    </Container>
+  );
+}
+
+export default ActorsPage;
