8e83c027226773538329c4515bfae4966e4e061e
nirch <nirh2@yahoo.com>
2025-08-17 09:24:17 +0300

    navbar cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,24 +13,6 @@ const links = [
 
 export function Navbar() {
   const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
-
-  const items = links.map((link) => (
-    <a
-      key={link.label}
-      href={link.link}
-      className={classes.link}
-      data-active={active === link.link || undefined}
-      onClick={(event) => {
-        event.preventDefault();
-        setActive(link.link);
-      }}
-    >
-      {link.label}
-    </a>
-  ));
-
-  console.log(classes);
 
   return (
     <header className={classes.header}>
